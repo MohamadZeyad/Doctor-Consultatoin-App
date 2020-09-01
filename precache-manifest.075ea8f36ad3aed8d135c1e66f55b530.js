9e72dae62db70a584e59f07a802a9c4caef5335a
Mohamad Zeyad <m-store@M-Stores-MacBook.local>
2020-09-01 20:30:39 +0300

    Updates

---
+++ precache-manifest.075ea8f36ad3aed8d135c1e66f55b530.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10d016ec1439448a7736b7de62164dd6",
+    "url": "/Doctor-Consultatoin-App/index.html"
+  },
+  {
+    "revision": "336f6ec4ec2d82f48c16",
+    "url": "/Doctor-Consultatoin-App/static/css/main.4d978e7f.chunk.css"
+  },
+  {
+    "revision": "edc7e5f906518f8d6b6e",
+    "url": "/Doctor-Consultatoin-App/static/js/2.8545d14c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Doctor-Consultatoin-App/static/js/2.8545d14c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "336f6ec4ec2d82f48c16",
+    "url": "/Doctor-Consultatoin-App/static/js/main.2cc0b86e.chunk.js"
+  },
+  {
+    "revision": "d02346016267358a83a9",
+    "url": "/Doctor-Consultatoin-App/static/js/runtime-main.748a2273.js"
+  },
+  {
+    "revision": "729687b89eaf4c2fbb2aa031651a4b40",
+    "url": "/Doctor-Consultatoin-App/static/media/search.729687b8.svg"
+  }
+]);
