dd9949490bf45f34c0f09b39c84229dbfb409910
Mohamad Zeyad <m-store@M-Stores-MacBook.local>
2020-09-06 00:46:06 +0300

    adding an image to card component

--- src/App.js
+++ src/App.js
@@ -33,8 +33,11 @@ function App() {
       <CategoriesText>Doctors</CategoriesText>
       <DoctorListItem></DoctorListItem>
       <img src="https://filedn.com/ltOdFv1aqz1YIFhf4gTY8D7/ingus-info/BLOGS/Photography-stocks3/stock-photography-slider.jpg"></img>
-      <img src="https://lh3.googleusercontent.com/qfh_mX-af4SwkI9lE-ofYGDnsHiNpon2cR0K3cXy1eVStZJK9GJJDbh5a2n3qwXyMpm4rM3LdhmtxKJzl_gBJko6zCwfUjWw8b2itUoY7-tTLPtdpUCBchpCsVWrbi_DNm3ME3H4u1_HLft7qURt5By4hB-lnKtGd50Cluq0WRO1OxE0sgiqqGsEn5VwTY4xb7FnK713fRAWNGQIpMVS9bUV3SQjYgqfT1njlXM35L8GDNytWUJaZ3FqwLuByjsMwkSV3ze6dp2TiYf7jrTof9iOPbSFKcJK3ZdDfd3k1Gy3VjjEGvzPBcD8DOzvRKFyg7TEXQ9xd0Eovqv8BdtY1_UlP41CXynOrrlSsLQPNv32B3xyaaqsPOTo023RF1iFHi_AhQTq_fbj-R7Tfg3Bvc5mSYkSUPP6XtWD3UPj_j9rtSgtvVjA2vq-d9BcUT8Ich3WkjdtFZxLE6QqkR02MfoPU5tFO8vQKGAiZ-XrcjQ-qZMiZpX__zGwAidks2Hou2vmo3_4cC20kboQ7zBPClQ2LL8Ko7M9tod5-cus0Zz-hu5cFFxHEQ5M6tCHteW6YewgPTDtaNOsZ4yaRtGzj_8-HDV7XxTdV3btWBPAwGPg7lv-6iYmSSWMahymuvahl-bAa0PltWcyuoRKCUraTgAI74SkQKLHcQC_UihlValMO3QZL_bO8Cl0y5ORtQ=w537-h664-no?authuser=0"></img>
-      <p>added an image</p>
+      <img
+        src="https://i.ibb.co/whsd8sX/doc-all-by-himself.png"
+        alt="doc-all-by-himself"
+        border="0"
+      ></img>
     </div>
   );
 }
