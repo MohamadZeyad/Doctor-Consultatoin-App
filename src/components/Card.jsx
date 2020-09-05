1f27234c43b64103b4688b3867057a4cd0180093
Mohamad Zeyad <m-store@M-Stores-MacBook.local>
2020-09-06 00:36:08 +0300

    adding an image to card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -21,7 +21,7 @@ const Image = styled.div`
   margin-left: 20px;
   /* background-color: red; */
   height: 100%;
-  background-image: url("https://lh3.googleusercontent.com/hlBSLg6wsi_GQ7cCKMnUJpPAWDUca1c8j05JhSnIcKbpQrFh_onZp_-4b9LZcrp672vcvcL6NlzwrhGiShLRpAJHObOvalZH62h6_h35MhmSYIAqh3WQmtrU5rjPsxy_Pg-3rPGB7NB5mqD4GkVcvhHbHsEZfUD6-cb4zHXCpJuyPrmgaN5qUMImxvNT5AexQtVEezGsa0UPrVfYvRnO5L2_jrlQsi40kgyP-tQurQ98jbItgGHI20zs3NIYnJJKlGFcUUbjTULSAN3N-MwmW0wijet-pHQvGAWG2ISk3YDNHJAeMMxPvnYWz4jDuEP4HZYs4s4o4x2hgWyOeMoWUaHfA6wQp-TetSn9-cGe1c8Z3qC1kepX227zXO3Y1OeGeWi6OxIgRrvbbdfO2tOOaz4Lx6-wlH4qBzDLqKbe6alDTbfS5q0K9u9sX6kGWsNevXANWjurHbmzF6mzGrP4qHofmZKNJYLlMbbXYQSQ8AA74Zj8tWSwpTVQAQBgZS25GojkNtSZk9ZftvyPAS71u5royY2qrVFnALDEsZCL5lX4ERlTwSHl4TtmRKWrZyG4DxCCnsxw8belLRk5exl4wp1jdyAMr43Gr4x30cr-QvCAeuy_gyPLsMyVFfxzAPYGa51RozNSgzbGubPIosVLMz7zRtHlcEdNJCUDZA34QAW9Md4jOohd4x6U2vjENw=w537-h664-no?authuser=0");
+  background-image: url("https://photos.google.com/share/AF1QipOoLInNKOkihDGWFHcS0tJROLfORw-V804cvxlw0DA4StShcbvt5fNSXeTI073-Eg?key=OGVEOW1oWTRGSmRfVFdXdHJRNkV4QXI5bG8ybGxn");
   background-repeat: no-repeat;
 
   background-size: 120px 170px;
