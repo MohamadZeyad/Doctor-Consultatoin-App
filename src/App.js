7f911dcc2b6639f12ddf27822871b512d25d548a
Mohamad Zeyad <m-store@M-Stores-MacBook.local>
2020-09-09 01:54:27 +0300

    implemented a basic routing system for the app and removed default underline styles of an <a> tag

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { useState } from "react";
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
 import "./App.css";
 import styled from "styled-components";
 
-import Div from "./components/div";
 import MenuIcon from "./components/MenuIcon";
 import SearchField from "./components/SearchField";
 import Nav from "./components/nav";
 import Card from "./components/Card";
 import DoctorListItem from "./components/DoctorListItem";
 import ExpandedMenu from "./components/ExpandedMenu";
+import DoctorDetailsPage from "./Pages/doctorDetailsPage";
 const HeaderText = styled.h1`
   width: 70vw;
   margin-left: 20px;
@@ -21,33 +28,75 @@ const CategoriesText = styled.h2`
   margin-top: 40px;
   margin-left: 20px;
 `;
-// const Menu = styled.div`
-//   margin-top: -0px;
-//   position: absolute;
-//   height: 600px;
-//   width: 200px;
-//   background-color: lightgrey;
-// `;
 
-function App() {
-  const [toggle, setToggle] = useState(false);
+// function App() {
+//   const [toggle, setToggle] = useState(false);
+//   return (
+//     <div className="App">
+//       <div onClick={() => setToggle(!toggle)}>
+//         <MenuIcon isexpanded={toggle}></MenuIcon>
+//       </div>
+//       {toggle ? <ExpandedMenu></ExpandedMenu> : null}
+
+//       <HeaderText>Find Your Consultation</HeaderText>
+//       <SearchField></SearchField>
+//       <CategoriesText>Categories</CategoriesText>
+//       <Nav></Nav>
+//       <Card></Card>
+
+//       <CategoriesText>Doctors</CategoriesText>
+
+//       <DoctorListItem></DoctorListItem>
+//     </div>
+//   );
+// }
+// export default App;
+
+function About() {
   return (
-    <div className="App">
-      <div onClick={() => setToggle(!toggle)}>
-        <MenuIcon isexpanded={toggle}></MenuIcon>
-      </div>
-      {toggle ? <ExpandedMenu></ExpandedMenu> : null}
-
-      <HeaderText>Find Your Consultation</HeaderText>
-      <SearchField></SearchField>
-      <CategoriesText>Categories</CategoriesText>
-      <Nav></Nav>
-      <Card></Card>
-
-      <CategoriesText>Doctors</CategoriesText>
-      <DoctorListItem></DoctorListItem>
+    <div>
+      <DoctorDetailsPage></DoctorDetailsPage>
+      <Link to="/">GO BACK</Link>
     </div>
   );
 }
 
+function Home() {
+  const [toggle, setToggle] = useState(false);
+  return (
+    <div>
+      {/* <h1>Home</h1> */}
+      <div className="App">
+        <div onClick={() => setToggle(!toggle)}>
+          <MenuIcon isexpanded={toggle}></MenuIcon>
+        </div>
+        {toggle ? <ExpandedMenu></ExpandedMenu> : null}
+        <HeaderText>Find Your Consultation</HeaderText>{" "}
+        <SearchField></SearchField>
+        <CategoriesText>Categories</CategoriesText>
+        <Nav></Nav>
+        <Card></Card>
+        <CategoriesText>Doctors</CategoriesText>
+        <Link to="/about">
+          <DoctorListItem></DoctorListItem>
+        </Link>
+      </div>
+    </div>
+  );
+}
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/about">
+          <About></About>
+        </Route>
+
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
 export default App;
---
+++ src/Pages/doctorDetailsPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const doctorDetailsPage = () => {
+  return (
+    <div>
+      <h1>doctorDetailsPage</h1>
+    </div>
+  );
+};
+
+export default doctorDetailsPage;
